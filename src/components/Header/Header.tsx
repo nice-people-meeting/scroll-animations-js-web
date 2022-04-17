efa0cf11cde0f5aa5a7fe066ce038db4fa1fa568
장동현 <54137044+ww8007@users.noreply.github.com>
2022-04-18 00:18:48 +0900

    feat: Add github, page logo
    Co-authored-by: JHK <kjhg478@gmail.com>
    Co-authored-by: hyebin-Hwang <hyebin-Hwang@users.noreply.github.com>
    Co-authored-by: Jinhyun Park <developerjhp@gmail.com>
    Co-authored-by: Woojin Jeong <woobba94@users.noreply.github.com>
    Co-authored-by: Bora Choi <borachoidev@users.noreply.github.com>

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,6 @@
 import { AnimationList } from './AnimationList';
 import Button from './Button';
-import { ButtonWrapper, HeaderWrapper, LogoContainer, ToNpmContainer, ToNpmDiv, GihubLogo } from './styled';
+import { ButtonWrapper, HeaderWrapper, LogoContainer, ToNpmContainer, ToNpmDiv, GithubLogo } from './styled';
 import { ReactComponent as GithubIcon } from '../../assets/github.svg';
 import { ReactComponent as Logo } from '../../assets/logo.svg';
 
@@ -12,21 +12,15 @@ export default function Header({ curAnimationHandler }: Prop) {
   return (
     <HeaderWrapper>
       <ToNpmContainer>
-        <ToNpmDiv>
+        <ToNpmDiv />
+        <GithubLogo style={{ justifySelf: 'flex-end' }}>
           <a href="https://github.com/nice-people-meeting/scroll-animation-js" target="_blank" rel="noreferrer">
-            {/* &gt; Go To Npm */}
-            <Logo width={40} height={40} fill="#202020" />
-          </a>
-        </ToNpmDiv>
-        <GihubLogo style={{ justifySelf: 'flex-end' }}>
-          <a href="https://github.com/nice-people-meeting/scroll-animation-js" target="_blank" rel="noreferrer">
-            {/* &gt; Go To Npm */}
             <GithubIcon width={40} height={40} fill="#fff" />
           </a>
-        </GihubLogo>
+        </GithubLogo>
       </ToNpmContainer>
       <LogoContainer>
-        <h1>Scroll Animations JS</h1>
+        <Logo width={400} height={56} />
       </LogoContainer>
       <ButtonWrapper>
         {AnimationList.map(el => {
