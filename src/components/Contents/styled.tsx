0f990572ecc2a5a47f4baa2981eb476b672aa67a
장동현 <54137044+ww8007@users.noreply.github.com>
2022-04-18 00:18:06 +0900

    style: Fix conents style
    Co-authored-by: JHK <kjhg478@gmail.com>
    Co-authored-by: hyebin-Hwang <hyebin-Hwang@users.noreply.github.com>
    Co-authored-by: Jinhyun Park <developerjhp@gmail.com>
    Co-authored-by: Woojin Jeong <woobba94@users.noreply.github.com>
    Co-authored-by: Bora Choi <borachoidev@users.noreply.github.com>

--- src/components/Contents/styled.tsx
+++ src/components/Contents/styled.tsx
@@ -2,82 +2,201 @@ import styled from 'styled-components';
 
 // Contents
 export const ContentsWrapper = styled.div`
-  position: relative;
   display: flex;
-  justify-content: space-evenly;
-  width: 100%;
-  height: 48vh;
-  margin-bottom: 50px;
+  flex: 1;
+  flex-direction: column;
+
+  align-items: center;
 `;
+
+export const CodeWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  @media (max-width: 870px) {
+    flex-direction: column;
+  }
+`;
+
 export const SubTitle = styled.p`
   font-size: 26px;
-  color: #8a8a8a;
+  color: #f3f1f1;
   font-weight: 700;
-  margin-bottom: 10px;
+  margin: 2rem 0;
+  a {
+    margin-left: 10px;
+    position: relative;
+    padding-top: 60px;
+  }
+  a span {
+    top: 50px;
+    position: absolute;
+    width: 30px;
+    height: 50px;
+    border: 2px solid #fff;
+    border-radius: 50px;
+    box-sizing: border-box;
+  }
+  a span::before {
+    position: absolute;
+    top: 10px;
+    left: 50%;
+    content: '';
+    width: 6px;
+    height: 6px;
+    margin-left: -3px;
+    background-color: #fff;
+    border-radius: 100%;
+    -webkit-animation: sdb 2s infinite;
+    animation: sdb 2s infinite;
+    box-sizing: border-box;
+  }
+  @-webkit-keyframes sdb {
+    0% {
+      -webkit-transform: translate(0, 0);
+      opacity: 0;
+    }
+    40% {
+      opacity: 1;
+    }
+    80% {
+      -webkit-transform: translate(0, 20px);
+      opacity: 0;
+    }
+    100% {
+      opacity: 0;
+    }
+  }
+  @keyframes sdb {
+    0% {
+      transform: translate(0, 0);
+      opacity: 0;
+    }
+    40% {
+      opacity: 1;
+    }
+    80% {
+      transform: translate(0, 20px);
+      opacity: 0;
+    }
+    100% {
+      opacity: 0;
+    }
+  }
 `;
 export const Content = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: start;
   align-items: center;
-  width: 80%;
-  height: 100%;
-  max-width: 600px;
 `;
 
 // CodeBox
 export const CodeBoxWrapper = styled.code`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  margin-top: 20px;
   div {
-    width: 80%;
     justify-content: center;
     align-items: flex-start;
+    margin-right: 20px;
+    width: 600px;
     :first-child {
       padding: 20px;
     }
+    @media (max-width: 870px) {
+      margin-right: 0;
+    }
+    @media (max-width: 650px) {
+      width: 425px;
+    }
   }
+`;
+
+export const OptionContainerWrapper = styled.div`
+  background: #282a36;
+  color: #f3f1f1;
+  padding: 1.2rem;
+  border-radius: 2px;
+  @media (max-width: 870px) {
+    width: 100%;
+    margin-top: 20px;
+  }
+`;
+
+export const OptionTitle = styled.p`
+  padding-bottom: 0.7rem;
+`;
+
+export const OptionBoxWrapper = styled.div`
+  padding: 0.3rem 0;
+  font-size: 0.9rem;
 `;
 
 // AnimationBox
 export const AnimationBoxWrapper = styled.div`
   display: flex;
-  background-color: #ddd;
+  background-color: #2f3846;
   justify-content: center;
   align-items: flex-start;
-  width: 100%;
-  height: 100%;
+  width: 300%;
+  border-top-left-radius: 1rem;
+  border-bottom-left-radius: 1rem;
+
+  height: 40rem;
   overflow: hidden;
   overflow-y: scroll;
+  margin-bottom: 5rem;
+
+  &::-webkit-scrollbar {
+    width: 8px;
+    height: 8px;
+    border-radius: 1rem;
+    background: rgba(255, 255, 255, 0.4);
+  }
+  &::-webkit-scrollbar-thumb {
+    background: rgba(0, 0, 0, 0.3);
+    border-radius: 1rem;
+  }
 `;
 
-export const TestAniamtionDiv = styled.div`
-  width: 200px;
-  height: 200px;
-  margin-bottom: 100px;
-  border: 1px solid black;
-  margin-left: auto;
-  margin-right: auto;
-  background-color: #ececec;
-`;
 export const AnimationBackground = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   width: 100%;
-  height: 400%;
+
   div {
-    width: 50px;
-    height: 50px;
-    margin-bottom: 100px;
+    width: 10rem;
+    height: 10rem;
+    border-radius: 10px;
+    margin-bottom: 10rem;
     border: 1px solid black;
     margin-left: auto;
     margin-right: auto;
-    background-color: #ececec;
+    background-color: #325cba;
+    align-items: center;
+    text-align: center;
+    display: flex;
+    :first-child {
+      margin-top: 5rem;
+      background-color: #6f0000;
+    }
+    :nth-child(2) {
+      background-color: #ff8c00;
+    }
+    :nth-child(3) {
+      background-color: #cccc00;
+    }
+    :nth-child(4) {
+      background-color: #006400;
+    }
+    :nth-child(6) {
+      background-color: #000080;
+    }
+    :nth-child(7) {
+      background-color: #483d8b;
+    }
+  }
+  span {
+    color: white;
+    font-weight: bold;
   }
 `;
 
@@ -85,4 +204,8 @@ export const AnimationBackground = styled.div`
 
 export const RangeBar = styled.div`
   display: flex;
+  padding: 0.3rem 0;
+  input {
+    width: 100%;
+  }
 `;
